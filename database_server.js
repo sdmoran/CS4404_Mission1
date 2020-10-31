bee69b8e6dc54aca6feabaf4b3fef3d59cc9d543
Sam Moran <smoran@wpi.edu>
2020-10-31 00:35:23 -0700

    Finished functionality, added python script to randomly vote for all userIDs

--- database_server.js
+++ database_server.js
@@ -1,6 +1,10 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const { uuid } = require('uuidv4')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs')
+const numTestUsers = 100
 const sequelize = new Sequelize('sqlite::memory:')
+const candidates = ["Reebok Obama", "Mittens Rhombus", "Vermin Supreme", "Sheev Palpatine", "John McCain"]
+
 
 class User extends Model {}
 User.init({
@@ -31,18 +35,36 @@ sequelize.sync({ force: true})
         console.log(e)
     })
 
+// Create test users
 function createTestUsers() {
-    for(let i = 0; i < 5; i++) {
-        const id = uuid();
-        console.log("UUID: ", id)
+    // Clear file so we don't have UUIDs from previous runs
+    fs.truncate('userIDs.txt', 0, function(){console.log('Cleared userIDs.txt')})
+    let writeStream = fs.createWriteStream('userIDs.txt', {
+        flags: 'a'
+    });
+    for(let i = 0; i < numTestUsers; i++) {
+        const id = uuidv4();
+        writeStream.write(`${id}\n`)
         User.create({id: id})
     }
+    writeStream.end();
 }
 
 // Express server
 const express = require('express')
+const bodyParser = require('body-parser')
+var corsMiddleware = require('cors')
 const app = express()
+
+// Parse application/json
+app.use(bodyParser.json())
+
 const port = 5000
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 
 // Get results, grouped by candidate
 app.get('/results', (req, res) => {
@@ -58,11 +80,26 @@ app.get('/results', (req, res) => {
     })
 })
 
+// Send list of candidates
+app.get('/candidates', corsMiddleware(corsOptions), (req, res) => {
+    res.send(JSON.stringify(candidates))
+})
+
+// Post requests needs options
+app.options('/vote', corsMiddleware(corsOptions))
+
 // Attempt to add vote to DB
-app.post('/vote', (req, res) => {
+app.post('/vote', corsMiddleware(corsOptions), (req, res) => {
     const id = req.param('id')
-    const candidate = req.param('candidate').toLowerCase()
+    var candidate = req.param('candidate')
+
     console.log(`ID: ${id}, Candidate: ${candidate}`)
+
+    if(!candidate || !id) {
+        res.status(403).send("Vote failed")
+        return
+    }
+    candidate = candidate.toLowerCase()
 
     User.findByPk(id)
     .then((rec) => {
---
+++ vote.py
@@ -0,0 +1,12 @@
+import requests, random, json
+
+url = "http://localhost:5000"
+
+candidates = requests.get(url + '/candidates').json()
+
+with open('userIDs.txt', 'r') as f:
+    for line in f:
+        vote = {'id': line.strip(), 'candidate': random.choice(candidates)}
+        print(vote)
+        x = requests.post(url + '/vote', headers={'content-type': 'application/json'}, data=json.dumps(vote))
+        print(x.text)
